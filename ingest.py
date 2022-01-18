640aa27b097f7b5a80eaeb2cf19a6551c52e6dc0
Sander Furre <sanderfu@stud.ntnu.no>
2022-01-18 11:49:26 +0100

    Add ingestion system to register ENCs

---
+++ ingest.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+#Description: The purpose of this script is to ingest .000 ENC files, register them for easy lookup and store them ina defined folder structure
+
+from osgeo import ogr, osr, gdal
+import sys, getopt, os, glob, csv
+
+def main():
+    input = "./ingest"
+    output = "./registered"
+    targetPattern = "*.000"
+    enc_paths = glob.glob(os.path.join(input,targetPattern))
+
+    ds_list = []
+    for enc_path in enc_paths:
+        ds_list.append(gdal.OpenEx(enc_path))
+
+    if any(ds_list)==None or len(ds_list)==0:
+        print("Error open source file(s). Exiting.")
+        sys.exit(1)
+    
+    column_names = ["filename","DSID_INTU","min_long","max_long","min_lat","max_lat"]
+    existing_encs = []
+    with open(os.path.join(output,"index.txt"),mode="r",newline='') as index_file:
+        index_reader = csv.reader(index_file)
+        for row in index_reader:
+            if row[0]=="filename":
+                continue
+            existing_encs.append(row[0])
+
+
+    rows_list = []
+    for i, ds in enumerate(ds_list):
+        row = []
+
+        #Check if dataset file already is registered
+        if enc_paths[i].split(sep="/")[-1] in existing_encs:
+            print("Warning: ",enc_paths[i].split(sep="/")[-1], " already registered. Skipping.")
+            continue
+        else:
+            print("From: ", )
+            os.rename(enc_paths[i],os.path.join(output,enc_paths[i].split(sep="/")[-1]))
+
+        #Add filename and intended usage (INTU)
+        row.append(enc_paths[i].split(sep="/")[-1])
+        row.append(ds.GetLayerByName("DSID").GetFeature(0).GetFieldAsString("DSID_INTU"))
+
+        #Add latitude and longitude of the area extent
+        row.extend(ds.GetLayerByName("M_COVR").GetExtent())
+        print(row)
+
+        rows_list.append(row)
+    
+    existing_encs = []
+    with open(os.path.join(output,"index.txt"),mode="r",newline='') as index_file:
+        index_reader = csv.reader(index_file)
+        for row in index_reader:
+            if row[0]=="filename":
+                continue
+            existing_encs.append(row[0])
+
+    with open(os.path.join(output,"index.txt"),mode="a") as index_file:
+        index_writer = csv.writer(index_file)
+        index_writer.writerows(rows_list)
+    
+
+
+
+    
+
+
+
+
+main()
---
+++ ingest/readme.md
@@ -0,0 +1,5 @@
+# Instructions
+
+1. Add new ENCs to the ingest folder
+2. Run the ingest.py script to register these ENCs in the system. This will remove all accepted ENCs from the ingest folder to the correct storage location.
+
